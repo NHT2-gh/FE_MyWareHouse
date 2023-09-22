503c4ced20eeb4c8881370e90def11e537f2cd35
NHT2 <thanhnn489@gmail.com>
2023-09-22 17:58:20 +0700

    request

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,75 +1,36 @@
-/*==============INCLUDE HEADER=============*/
-// function includeHTML() {
-//     var z, i, elmnt, file, xhttp;
-//     /* Loop through a collection of all HTML elements: */
-//     z = document.getElementsByTagName("*");
-//     for (i = 0; i < z.length; i++) {
-//       elmnt = z[i];
-//       /*search for elements with a certain atrribute:*/
-//       file = elmnt.getAttribute("w3-include-html");
-//       if (file) {
-//         /* Make an HTTP request using the attribute value as the file name: */
-//         xhttp = new XMLHttpRequest();
-//         xhttp.onreadystatechange = function() {
-//           if (this.readyState == 4) {
-//             if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-//             if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-//             /* Remove the attribute, and call this function once more: */
-//             elmnt.removeAttribute("w3-include-html");
-//             includeHTML();
-//           }
-//         }
-//         xhttp.open("GET", file, true);
-//         xhttp.send();
-//         /* Exit the function: */
-//         return;
-//       }
-//     }
-//   }
-
-/*=============== DARK LIGHT THEME ===============*/
-const themeButton = document.getElementById("theme-button");
-const darkTheme = "dark-theme";
-const iconTheme = "bx-sun";
-
-// Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem("selected-theme");
-const selectedIcon = localStorage.getItem("selected-icon");
-
-// We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () =>
-  document.body.classList.contains(darkTheme) ? "dark" : "light";
-const getCurrentIcon = () =>
-  themeButton.classList.contains(iconTheme) ? "bx bx-moon" : "bx bx-sun";
-
-// We validate if the user previously chose a topic
-if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
-    darkTheme
-  );
-  themeButton.classList[selectedIcon === "bx bx-moon" ? "add" : "remove"](
-    iconTheme
-  );
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
 }
+function ready() {}
 
-// Activate / deactivate the theme manually with the button
-themeButton.addEventListener("click", () => {
-  // Add or remove the dark / icon theme
-  document.body.classList.toggle(darkTheme);
-  themeButton.classList.toggle(iconTheme);
-  // We save the theme and the current icon that the user chose
-  localStorage.setItem("selected-theme", getCurrentTheme());
-  localStorage.setItem("selected-icon", getCurrentIcon());
+// ACTIVE SIDEBAR
+const allSideMenu = document.querySelectorAll("#sidebar .side-menu.top li a");
+
+allSideMenu.forEach((item) => {
+  const li = item.parentElement;
+
+  item.addEventListener("click", function () {
+    allSideMenu.forEach((i) => {
+      i.parentElement.classList.remove("active");
+    });
+    li.classList.add("active");
+  });
 });
-// ========CHECK========//
-
-function isValidEmail(email) {
-  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return emailRegex.test(email);
+// TOGGLE SIDEBAR
+const menuBar = document.querySelector("#content nav .bx.bx-menu");
+const sidebar = document.getElementById("sidebar");
+if (menuBar) {
+  menuBar.addEventListener("click", function () {
+    sidebar.classList.toggle("hide");
+  });
 }
 
-function isValidPhoneNumber(phoneNumber) {
-  var phoneRegex = /^\d{10}$/;
-  return phoneRegex.test(phoneNumber);
-}
+// DETAIL
+const div_detail = document.getElementById("detail");
+window.onclick = function (event) {
+  if (event.target == div_detail) {
+    div_detail.style.display = "none";
+  }
+};
